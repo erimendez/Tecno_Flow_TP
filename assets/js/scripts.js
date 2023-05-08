0e980c1c65f6177e48ce78572f98b37210d86f96
erimendez <erichi_san@yahoo.com.ar>
2023-05-07 21:00:55 -0300

    Ordenando el repo
    Eliminando los estilos replicados

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,13 +1,42 @@
 let header = `
-<h1> Aca va el logo y demas datos</h1>
+<div class="logo">
+<img src="./assets/media/img/tecnoflow.jpg" alt="aca va el logo">
+</div>
+<div class="texto">
+<h1>Tecnologia que no para</h1>
+</div>
+<div class="contenedorbuscador">
+<div class="buscador">
+<input type="text" id="buscarproducto" placeholder="Que tecnologia necesitas hoy?">
+</div>
+<div class="lupa">
+<i class="fa-sharp fa-solid fa-magnifying-glass fa-beat"></i>
+</div>
+</div>
+<div >
+<h1> iconos lindos</h1>
+</div>
 <nav>
 <ul>
-  <li><a href="index.html">Index</a></li>
-  <li><a href="#">Nosotros</a></li>
-  <li><a href="#">Servicios</a></li>
-  <li><a href="sucursales.html">Sucursales</a></li>
-  <li><a href="contacto.html">Contáctanos</a></li>
-</ul>
-</nav>
+<li>
+<a href="#">Nosotros</a>
+</li></ul><ul><li>
+            <a href="#">Producto</a>
+            <ul>
+              <li><a href="">Submenu1</a></li>
+              <li><a href="">Submenu2</a></li>
+              <li><a href="">Submenu3</a></li>
+              <li><a href="">Submenu4</a></li>
+            </ul></li></ul><ul>
+            <li>
+              <a href="#">Acerca</a>
+            </li></ul><ul>
+        <li>
+            <a href="#">Sucursales</a>
+        </li></ul><ul>
+      <li>
+          <a href="#">Promociones</a>
+      </li>
+  </ul></nav></div>
 `
 document.getElementById("idheader").innerHTML = header
